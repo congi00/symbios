84497d4a34c2c046df7273199b5bf7066269c576
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-06-04 21:31:06 +0200

    [Text content home page]

--- src/components/InfosHomeText.jsx
+++ src/components/InfosHomeText.jsx
@@ -7,6 +7,14 @@ function InfosHomeText() {
     return (
       <div className="infos-home-text">
         <div className="infos-home-title">{mainPage.mainTitle}</div>
+        <div className="infos-home-subtitles">
+            <div className="infos-home-motto">
+                {mainPage.motto}
+            </div>        
+            <div className="infos-home-motto-subtitle">
+                {mainPage.mottoSubTitle}
+            </div>     
+        </div>
       </div>
     );
   }
